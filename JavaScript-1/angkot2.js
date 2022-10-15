5067e883a95847bb33dd71bbcd16783c62155035
SRomadiah <diyah1888@gmail.com>
2022-10-15 21:05:04 +0800

    next step

--- JavaScript-1/angkot2.js
+++ JavaScript-1/angkot2.js
@@ -1,10 +1,10 @@
 var jumlahAngkot = 10;
 var noAngkot = 1;
 var angkotBeroperasi = 6;
-while (noAngkot <= 6) {
-  console.log("Angot No. " + "" + angkotBeroperasi + " beroperasi dengan baik");
+while (noAngkot <= angkotBeroperasi) {
+  console.log("Angot No. " + angkotBeroperasi + " beroperasi dengan baik");
   noAngkot++;
-  for (var noAngkot = 1; noAngkot >= 7; noAngkot++) {
-    console.log("Angkot No. " + noAngkot + " tidak beroperasi dengan baik");
-  }
+}
+for (noAngkot = angkotBeroperasi + 1; noAngkot >= jumlahAngkot; noAngkot++) {
+  console.log("Angkot No. " + noAngkot + " tidak beroperasi dengan baik");
 }
